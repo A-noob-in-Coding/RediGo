fa14dc62c68bff11cc7ce2cdf7acdd4513719fc5
A-noob-in-Coding <naeemabdulrehman77@gmail.com>
2025-06-24 17:44:28 +0500

    Completed Project

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"io"
+	"net/http"
+)
+
+var (
+	ctx = context.Background()
+	rdb *redis.Client
+)
+var Origin *string
+var Port *string // default port
+func handleReq(v http.ResponseWriter, r *http.Request) {
+	requestURL := *Origin
+	header := v.Header()
+	reqHeaders := r.Header.Get("Cache-Required")
+	if r.URL.Path == "/redigo/clear" {
+		err := rdb.FlushDB(ctx).Err()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Cache Cleared Successfully\n")
+		return
+	}
+
+	if r.URL.Path == "/" {
+		requestURL += r.URL.Path[1:]
+	} else {
+		requestURL += r.URL.Path
+	}
+
+	result, err := rdb.Get(ctx, requestURL).Result()
+	if err == nil && reqHeaders == "yes" {
+		fmt.Printf("Cache Hit\n")
+		header.Set("X-Cache", "Hit")
+		io.WriteString(v, result)
+
+		return
+	} else {
+		fmt.Printf("Cache Miss\n")
+	}
+	resp, err := http.Get(requestURL)
+	if err != nil {
+		fmt.Println("could not get")
+	}
+	if resp.StatusCode == 404 {
+		io.WriteString(v, "ERROR 404, invalid api endpoint")
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	header.Set("X-Cache", "Miss")
+	_, _ = io.WriteString(v, string(body))
+
+	err = rdb.Set(ctx, requestURL, string(body), 0).Err()
+	if err != nil {
+		fmt.Println("Error storing in Redis:", err)
+	}
+}
+
+func main() {
+	Port = flag.String("port", "1234", "port number or proxy server to run")
+	Origin = flag.String("origin", "", "origin endpoint to forward requests")
+	flag.Parse()
+	if *Origin == "" {
+		fmt.Println("Set origin using -origin <origin>")
+		return
+	}
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	http.HandleFunc("/", handleReq)
+	fmt.Printf("Server running at Port %s\n", *Port)
+	http.ListenAndServe(":"+*Port, nil)
+}
+
